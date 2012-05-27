d4514acda3da9c975af74184522062ebcd396bd9
vot <vot@localhost>
2012-05-27 11:02:12 +0400

    [2.5] Russian language added

---
+++ 2.5/ru/adv/lang_cornerbanner.php
@@ -0,0 +1,32 @@
+<?php
+
+/**---
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_cornerbanner.php by Valery Votintsev at sources.ru
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = array
+(
+	'cornerbanner_name'		=> 'Глобальная реклама в нижнем правом углу страницы',
+	'cornerbanner_desc'		=> 'Блок рекламы отображается в нижнем правом углу на всех страницах. Если установлено несколько объявлений данного типа, то будет выводиться одно случайно выбранное объявление.<br />Value Analysis: The ability to demonstrate a clear position on the page, the page become the highest price and most suitable for commercial advertising or brand advertising types.',
+	'cornerbanner_index'		=> 'Главная',
+	'cornerbanner_fids'		=> 'Форум',
+	'cornerbanner_fids_comment'	=> 'Укажите разделы форума, где показывать рекламу (действует только для разделов форума)',
+	'cornerbanner_groups'		=> 'Сообщества',
+	'cornerbanner_groups_comment'	=> 'Укажите категории сообществ, где показывать рекламу (действует только для сообществ)',
+	'cornerbanner_animator'		=> 'Анимация',
+	'cornerbanner_animator_comment'	=> 'Установка режима анимации рекламы',
+	'cornerbanner_category'		=> 'Категории портала',
+	'cornerbanner_category_comment'	=> 'Размещение рекламы в конкретных категориях портала',
+	'cornerbanner_disableclose'	=> 'Закрыть рекламную ссылку',//'关闭广告的链接',
+	'cornerbanner_disableclose_comment' => '如果广告代码中已内置关闭操作，可以关闭系统预置的关闭链接',
+	'cornerbanner_show' => '显示',
+	'cornerbanner_hidden' => '隐藏',
+);
+
