0c5923ff5acbcac2c938d69702b8be6cc65f8299
vot <vot@localhost>
2012-05-27 11:06:15 +0400

    [2.5] Vietnamese language added

---
+++ 2.5/vn/lang_setting.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_template.php by Valery Votintsev at sources.ru
+ *
+ *      This file is automatically generate
+ */
+
+$lang = array (
+//---------------------------
+//vot: Titles from install_var.php, pre_common_nav table
+
+	'nav_1'		=> 'Portal',//'门户',//1,
+	'nav_2'		=> 'Diễn đàn',//'Forum',//'论坛',//2,
+	'nav_3'		=> 'Nhóm',//'Groups',//'群组',//3,
+	'nav_4'		=> 'Không gian',//'Space',//'家园',//4,
+	'nav_5'		=> 'Trò chơi',//'Applications',//'应用',//5,
+	'nav_6'		=> 'Ứng dụng',//'Plugins',//'插件',//6,
+	'nav_7'		=> 'Trợ giúp',//'Help',//'帮助',//7,
+	'nav_8'		=> 'Xếp hạng danh sách',//'RankList',//'排行榜',//8,
+	'nav_stat'	=> 'Thống kê',//'Statistics',//'站点统计',
+	'nav_report'	=> 'Báo cáo',//'Reports',//'举报',
+	'nav_archiver'	=> 'Lưu trữ',//'Archive',
+	'nav_mobile'	=> 'Phiên bản Mobile',//'Mobile',//'手机版',
+	'nav_blog'	=> 'Blogs',//'日志',
+	'nav_album'	=> 'Albums',//'相册',
+	'nav_share'	=> 'Chia sẻ',//'Shares',//'分享',
+	'nav_doing'	=> 'Trạng thái',//'Status',//'Doings',//'记录',
+	'nav_friend'	=> 'Bạn bè',//'Friends',//'好友',
+	'nav_thread'	=> 'Bài viết',//'Threads',//'帖子',
+	'nav_favorite'	=> 'Ưa thích',//'Favorites',//'收藏',
+	'nav_magic'	=> 'Đạo cụ',//'Magic',//'道具',
+	'nav_medal'	=> 'Huy chương',//'Medals',//'勋章',
+	'nav_task'	=> 'Nhiệm vụ',//'Tasks',//'任务',
+	'nav_sethomepage'	=> '<img src="static/image/common/home.gif" title="Đặt trang chủ">',//'设为首页',
+	'nav_setfavorite'	=> '<img src="static/image/common/fav.gif" title="Đặt Bookmark">',//'收藏本站',
+	'nav_uchome'		=> 'UCHome',//'',
+
+//	'nav_'		=> '',//'',
+
+);
+
+/* ToDo !!!!!!!!!!
+	'profile_fields'		=> array(
+		'mp.realname'		=> 'Real Name',//'姓名',
+		'mp.gender'		=> 'Gender',//'性别',
+		'mp.birthyear'		=> 'Birth year',//'生日(年)',
+		'mp.birthmonth'		=> 'Birth month',//'生日(月)',
+		'mp.birthday'		=> 'Birth Day',//'生日(日)',
+		'mp.bloodtype'		=> 'Blood type',//'血型',
+		'mp.affectivestatus'	=> 'Emotional category',//'情感状态',
+'mp.birthcountry'	=> 'Birth country',//'家乡(国)',
+		'mp.birthprovince'	=> 'Birth region',//'家乡(省)',
+		'mp.birthcity'		=> 'Birth city',//'家乡(市)',
+		'mp.resideprovince'	=> 'Reside region',//'居住地(省)',
+		'mp.residecity'		=> 'Reside city',//'居住地(市)'
+	),
+*/
