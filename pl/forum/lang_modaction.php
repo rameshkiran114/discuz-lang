2ca3a244741ffd31ef494ad1e41a39cd2b0df3e4
vot <vot@localhost>
2012-06-29 10:35:36 +0400

    Polish (PL) language pack added, by kaaleth

---
+++ pl/forum/lang_modaction.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_modaction.php by Valery Votintsev at sources.ru
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = array
+(
+
+	'EDT'	=> 'Edytuj',//'编辑',
+
+	'DEL'	=> 'Usuń',//'删除',
+	'DLP'	=> 'Delete reply',//'删除回复',
+	'DCM'	=> 'Usuń komentarz',//'删除点评',
+	'PRN'	=> 'Prune posts',//'批量删帖',
+	'UDL'	=> 'Undelete',//'反删除',
+
+	'DIG'	=> 'Wykop',//'加入精华',
+	'UDG'	=> 'Undigest',//'解除精华',
+	'EDI'	=> 'Limit digest',//'限时精华',
+	'UED'	=> 'Unlimit digest',//'解除限时精华',
+
+	'CLS'	=> 'Zamknij',//'关闭',
+	'OPN'	=> 'Otwórz',//'打开',
+	'ECL'	=> 'Limit Close',//'限时关闭',
+	'UEC'	=> 'Unlimit close',//'解除限时关闭',
+	'EOP'	=> 'Limit open',//'限时打开',
+	'UEO'	=> 'Unlimit open',//'解除限时打开',
+
+	'STK'	=> 'Przyklej',//'置顶',
+	'UST'	=> 'Odklej',//'解除置顶',
+	'EST'	=> 'Stick Time Limited',//'限时置顶',
+	'UES'	=> 'UnStick Time Limited',//'解除限时置顶',
+
+	'SPL'	=> 'Podziel',//'分割',
+	'MRG'	=> 'Scal',//'合并',
+
+	'HLT'	=> 'Wyróżnij',//'设置高亮',
+	'UHL'	=> 'Unhighlight',//'解除高亮',
+	'EHL'	=> 'Time-limited Highlight',//'限时高亮',
+	'UEH'	=> 'Remove time-limited Highlight',//'解除限时高亮',
+
+	'BMP'	=> 'Odśwież',//'提升',
+	'DWN'	=> 'Na dół',//'下沉',
+
+	'MOV'	=> 'Przenieś',//'移动',
+	'CPY'	=> 'Skopiuj',//'复制',
+	'TYP'	=> 'Typ',//'分类',
+
+	'RFD'	=> 'Refund',//'强制退款',
+
+	'MOD'	=> 'Moderate',//'审核通过',
+
+	'ABL'	=> 'Add Blog',//'加入文集',
+	'RBL'	=> 'Remove Blog',//'移除文集',
+
+	'PTS'	=> 'Kosz',//'推送主题',
+	'RFS'	=> 'Przywrócenie',//'解除推送',
+	'RMR'	=> 'Usunięcie nagrody',//'取消悬赏',
+	'BNP'	=> 'Ban',//'屏蔽帖子',
+	'UBN'	=> 'Zniesienie banicji',//'解除屏蔽',
+
+	'REC'	=> 'Recommend',//'推荐',
+	'URE'	=> 'Unrecommend',//'解除推荐',
+
+	'WRN'	=> 'ostrzeżenie',//'警告',
+	'UWN'	=> 'usunięcie ostrzeżenia',//'解除警告',
+
+	'SPA'	=> 'Add stamp',//'添加图章',
+	'SPD'	=> 'Remove stamp',//'撤销图章',
+
+	'SLA'	=> 'Add Icon',//'添加图标',
+	'SLD'	=> 'Remove Icon',//'撤销图标',
+
+	'REG'	=> 'Group recommend',//'群组推荐',
+
+	'PTA'	=> 'Wygeneruj artykuł',//'生成文章',
+
+	'MAG'	=> 'Magia',//'道具',
+
+	'REB'	=> 'Get promotion',//'推送',
+
+);
+
---
+++ pl/lang_setting.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_template.php by Valery Votintsev at sources.ru
+ *
+ *      This file is automatically generate
+ */
+
+$lang = array (
+//---------------------------
+//vot: Titles from install_var.php, pre_common_nav table
+
+	'nav_1'		=> 'Portal',//'门户',
+	'nav_2'		=> 'Forum',//'论坛',
+	'nav_3'		=> 'Grupy',//'群组',
+	'nav_4'		=> 'Space',//'家园',
+	'nav_5'		=> 'Applications',//'应用',
+	'nav_6'		=> 'Plugins',//'插件',
+	'nav_7'		=> 'Help',//'帮助',
+	'nav_8'		=> 'Ranking',//'排行榜',
+	'nav_9'		=> 'Follows',//'广播',
+	'nav_10'	=> 'Przewodnik',//'导读',
+	'nav_11'	=> 'Collections',//'淘帖',
+	'nav_12'	=> 'Blogi',//'日志',
+	'nav_13'	=> 'Albums',//'相册',
+	'nav_14'	=> 'Shares',//'分享',
+	'nav_15'	=> 'Moods',//'记录',
+
+	'nav_portal'	=> 'Portal',//'门户',
+	'nav_blog'	=> 'Blogi',//'日志',
+	'nav_album'	=> 'Albumy',//'相册',
+	'nav_share'	=> 'Shares',//'分享',
+	'nav_doing'	=> 'Moods',//'记录',
+	'nav_wall'	=> 'Wall',//'留言板',
+	'nav_group'	=> 'Grupy',//'群组',
+	'nav_ranklist'	=> 'Ranking',//'排行榜'
+	'nav_follow'	=> 'Follow',//'广播',
+	'nav_guide'	=> 'Przewodnik',//'导读',
+	'nav_collection'	=> 'Collections',//'淘帖',
+
+	'nav_stat'	=> 'Statystyki',//'站点统计',
+	'nav_report'	=> 'Raport',//'举报',
+	'nav_archiver'	=> 'Archiwum',
+	'nav_mobile'	=> 'Mobile',//'手机版',
+	'nav_friend'	=> 'Znajomi',//'好友',
+	'nav_thread'	=> 'Tematy',//'帖子',
+	'nav_favorite'	=> 'Ulubione',//'收藏',
+	'nav_magic'	=> 'Magia',//'道具',
+	'nav_medal'	=> 'Medale',//'勋章',
+	'nav_task'	=> 'Zadania',//'任务',
+	'nav_feed'	=> 'Informacje',//'动态',
+
+	'nav_sethomepage'	=> '<img src="static/image/common/home.gif" title="Ustaw jako strona domowa">',//'设为首页',
+	'nav_setfavorite'	=> '<img src="static/image/common/fav.gif" title="Dodaj do ulubionych">',//'收藏本站',
+	'nav_uchome'		=> 'UCHome',//'',
+
+//	'nav_'		=> '',//'',
+
+);
+
+/* ToDo !!!!!!!!!!
+	'profile_fields'		=> array(
+		'mp.realname'		=> 'Real Name',//'姓名',
+		'mp.gender'		=> 'Gender',//'性别',
+		'mp.birthyear'		=> 'Birth year',//'生日(年)',
+		'mp.birthmonth'		=> 'Birth month',//'生日(月)',
+		'mp.birthday'		=> 'Birth Day',//'生日(日)',
+		'mp.bloodtype'		=> 'Blood type',//'血型',
+		'mp.affectivestatus'	=> 'Emotional category',//'情感状态',
+'mp.birthcountry'	=> 'Birth country',//'家乡(国)',
+		'mp.birthprovince'	=> 'Birth region',//'家乡(省)',
+		'mp.birthcity'		=> 'Birth city',//'家乡(市)',
+		'mp.resideprovince'	=> 'Reside region',//'居住地(省)',
+		'mp.residecity'		=> 'Reside city',//'居住地(市)'
+	),
+*/
