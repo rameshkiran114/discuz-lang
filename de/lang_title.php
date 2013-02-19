5eafa189234debd3eddbaee93e505769f018728c
vvotintsev <vvotintsev@gmail.com>
2013-02-19 08:57:05 +0400

    Small fixes for DE language

--- de/lang_title.php
+++ de/lang_title.php
@@ -4,7 +4,7 @@
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
- *	Deutsch Language Pack
+ *	German Language Pack (Deutsch)
  *
  *      $Id: lang_title.php by Valery Votintsev at sources.ru
  *
